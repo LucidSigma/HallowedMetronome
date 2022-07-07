8c912705824593c895cd892597179b12c276eff8
Matthew Schafer <lucidsigma17@gmail.com>
2022-07-08 03:11:24 +1000

    Add item details and recipe

--- Content/Items/HallowedMetronome.cs
+++ Content/Items/HallowedMetronome.cs
@@ -1,4 +1,5 @@
 ﻿using Terraria;
+using Terraria.GameContent.Creative;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -7,9 +8,39 @@ namespace HallowedMetronomeMod.Content.Items
     public class HallowedMetronome
         : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            if (Main.dedServ)
+            {
+                return;
+            }
+
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+        }
+
         public override void SetDefaults()
         {
-            Item.CloneDefaults(ItemID.EncumberingStone);
+            Item.width = 30;
+            Item.height = 36;
+
+            Item.rare = ItemRarityID.Pink;
+
+            Item.maxStack = 1;
+            Item.value = Item.sellPrice(0, 5, 0, 0);
+        }
+
+        public override void AddRecipes()
+        {
+            Recipe recipe = CreateRecipe();
+
+            recipe.AddIngredient(ItemID.HallowedBar, 8);
+            recipe.AddIngredient(ItemID.SoulofLight, 10);
+            recipe.AddIngredient(ItemID.SoulofNight, 10);
+            recipe.AddIngredient(ItemID.Timer1Second, 1);
+
+            recipe.AddTile(TileID.MythrilAnvil);
+
+            recipe.Register();
         }
     }
 }
